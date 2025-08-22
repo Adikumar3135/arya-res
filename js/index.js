69659c95dd36253fb28e565fd4a482a759d5374f
Adikumar3135 <adikumar3135@gmail.com>
2025-08-22 17:55:48 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: "smooth" });
+}
+
+function viewMore(){
+    alert("Sorry, Right now it,s not avilable")
+}
+function oderNow(){
+    alert('Sorry,Currently This is Not avilable')
+}
+function menu(){
+    alert('Sorry,Currently This is Not avilable')
+}
